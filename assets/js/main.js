53a930d7c231c831da494c1a4a386ab34752a0df
borey chea <cheaborey25@gmail.com>
2025-01-04 00:53:54 +0700

    add firework

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,25 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const navMenu = document.querySelector(".nav_menu");
+// const menuToggle = document.querySelector(".menu-toggle");
+// const navMenu = document.querySelector(".nav_menu");
 const darkModeToggle = document.querySelector(".dark-mode-toggle");
 
-menuToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
+// menuToggle.addEventListener("click", () => {
+//   navMenu.classList.toggle("active");
+// });
 
 darkModeToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
+});
+
+const playModeToggle = document.querySelector(".play-mode-toggle");
+const firework = document.querySelector(".container-firework");
+
+playModeToggle.addEventListener("click", () => {
+  const icon = playModeToggle.querySelector("i");
+  if (icon.classList.contains("bx-play")) {
+    firework.style.display = "block";
+    icon.classList.replace("bx-play", "bx-pause"); // Switch to bx-pause
+  } else {
+    firework.style.display = "none";
+    icon.classList.replace("bx-pause", "bx-play"); // Switch back to bx-play
+  }
 });
